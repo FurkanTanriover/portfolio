baeb6e09d486750da733c6b2bed1b72969eb9dd3
Furkan Tanrıöver <furkan.tanrioveer@gmail.com>
2022-12-04 13:28:55 +0300

    create about section

---
+++ components/About.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable react/no-unescaped-entities */
+import { motion } from "framer-motion";
+import React from "react";
+
+type Props = {};
+
+function About({}: Props) {
+  return (
+    <motion.div
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:2}}
+    className="h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24  uppercase tracking-[20px] text-gray-500 text-2xl ">About</h3>
+      <motion.img
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        whileInView={{ x: 0, opacity: 1 }}
+        viewport={{ once: true }}
+        src="https://media-exp1.licdn.com/dms/image/C5603AQEKqLYh8XhVMg/profile-displayphoto-shrink_800_800/0/1623004462527?e=1675296000&v=beta&t=Pu_zmE7DqzclJoaoD1xzs0sXlAIwWz8lN_gpL2EBbVQ"
+        className="-mb-32 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-xl md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+      />
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold -mb-4">
+          Hakkımda <span className="underline decoration-[#f7ab0a]/50">küçük</span> bir özet
+        </h4>
+        <p className="text-base">
+          Ben Furkan, 1999 yılında İstanbul'da doğdum. 2021 yılında Marmara Üniversitesi Bilgisayar Programlama bölümünden mezun oldum.
+          Ardından Anadolu Üniversitesi Yönetim Bilişim Sistemlerine kayıt oldum. 2022 yılı haziran ayına kadar kendimi çeşitli eğitimlerle
+          geliştirdim, oyun geliştirme(unity, c#), mobil programlama(flutter, react native, dart, js) ve web(react, js). 2022 haziranda bir
+          firmada yazılım mühendisi olarak çalışmaya başladım. Burada (react, nextjs, hasura, firebase, react native) teknolojilerini
+          kullanarak geliştirmeler yaptım. Yazılım hayatım hakkında minik bir girişin sonrasında biraz hobilerimden bahsetmek isterim 🙂.
+          Boş zamanlarımı spor(futbol, fitness), dans(salsa, bachata), yazılım çalışmak, kitap okumak ve müzik dinlemek gibi aktivitelerle geçiriyorum.
+        </p>
+      </div>
+    </motion.div>
+  );
+}
+
+export default About;
--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -4,15 +4,15 @@ type Props = {};
 function BackgroundCircles({}: Props) {
   return (
     <motion.div
-      className="relative flex justify-center items-center"
+      className="relative bg-slate-400 flex justify-center items-center"
       initial={{ opacity: 0 }}
       animate={{ scale: [1, 2, 2, 3, 1], opacity: [0.1, 0.2, 0.4, 0.8, 1.0], borderRadius:["20%","20%","50%","80%","20%"] }}
       transition={{ duration: 2.5 }}
     >
-      <div className="border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping " />
-      <div className="border border-[#333333] rounded-full h-[300px] w-[300px] mt-52 animate-ping absolute" />
-      <div className="border border-[#333333] rounded-full h-[400px] w-[400px] mt-52 animate-ping absolute" />
-      <div className="border border-[#F7AB0A] opacity-10 rounded-full h-[650px] w-[650px] mt-52 animate-pulse absolute" />
+      <div className="border border-[#333333] opacity-80 rounded-full h-[200px] w-[200px] mt-52 animate-ping absolute" />
+      <div className="border border-[#333333] opacity-80 rounded-full h-[300px] w-[300px] mt-52 animate-ping absolute" />
+      <div className="border border-[#333333] opacity-80 rounded-full h-[400px] w-[400px] mt-52 animate-ping absolute" />
+      <div className="border border-[#F7AB0A] opacity-80 opacity-10 rounded-full h-[650px] w-[650px] mt-52 animate-pulse absolute" />
       <div />
     </motion.div>
   );
